97d9f7b7328fb8fb23f343ea0ce66710e9654405
Shreya Sharma <shreya_sharma1@baylor.edu>
2022-03-30 15:43:42 -0500

    Class 3/30

--- webscraping-COVID.py
+++ webscraping-COVID.py
@@ -2,9 +2,10 @@
 # pip install bs4 (for beautifulsoup - python tool to parse HTML)
 
 
+from asyncio.base_subprocess import BaseSubprocessTransport
+from turtle import title
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-
 
 ##############FOR MACS THAT HAVE ERRORS LOOK HERE################
 ## https://timonweb.com/tutorials/fixing-certificate_verify_failed-error-when-trying-requests_html-out-on-mac/
@@ -14,10 +15,60 @@ from bs4 import BeautifulSoup
 ##  > sudo "./Install Certificates.command"
 
 
-
 url = 'https://www.worldometers.info/coronavirus/country/us'
 # Request in case 404 Forbidden error
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+
+req = Request(url, headers = headers)
+webpage = urlopen(req).read()
+soup = BeautifulSoup(webpage, "html.parser")
+title = soup.title
+
+print(title.text)
+
+table_rows = soup.findAll("tr")
+
+state_death_ratio = ""
+state_best_testing = ""
+state_worst_testing = ""
+highest_death_ratio = 0.0
+high_test_ratio = 0.0
+Low_test_ratio = 100.0
+
+
+for row in table_rows[2:51]: 
+    td = row.findAll("td")
+    
+    state = td[1].text
+    total_cases = int(td[2].text.replace(',',''))
+    total_deaths = int(td[4].text.replace(',',''))
+    total_tested = int(td[10].text.replace(',',''))
+
+'''
+    print(f"State:{state}")
+    print(f"Total Cases: {total_cases}")
+    print(f"Total Deaths: {total_deaths}")
+    print(f"Total Tested: {total_tested}")
+
+    input()
+''' 
+
+    death_ratio = total_deaths/total_cases
+    test_ratio = total_cases/total_tested 
+
+    if death_ratio > highest_death_ratio:
+        state_death_ratio = state
+        highest_death_ratio = death_ratio
+
+    if test_ratio > high_test_ratio:
+        state_worst_testing = state
+        high_test_ratio = test_ratio
+
+    if test_ratio < low_test_ratio:
+        state_best_testing = state
+        low_test_ratio = test_ratio 
+
 
 
 
--- webscraping-tradingview.py
+++ webscraping-tradingview.py
@@ -16,9 +16,20 @@ url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 		
+req = Request(url, headers = headers)
+webpage = urlopen(req).read()
+soup = BeautifulSoup(webpage, "html.parser")
+title = soup.title
+
+print(title.text)
+
+companies = soup.findAll("span",attrs={"class":"tv-screener__description"})
+
+print(companies)
 
 
-
+for company in companies[:5]:
+    print(company.text.strip())
 
 
 
