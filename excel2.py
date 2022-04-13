057f3aa2d93015ae24c5de6d05959ffd1d776821
Shreya Sharma <shreya_sharma1@baylor.edu>
2022-04-13 15:38:14 -0500

    Class 4/13

--- excel2.py
+++ excel2.py
@@ -1,54 +1,45 @@
-######################### WORK BOOK ########################################
+import openpyxl as xl
 
-#create a new excel document
+wb = xl.load_workbook('2_example.xlsx')
 
-wb = xl.Workbook()
+sn = wb.sheetnames
 
-MySheet = wb.active
+print(sn)
 
-MySheet.title = "First Sheet"
+sheet1 = wb['Sheet1']
+cellA1 = sheet1["A1"]
 
+print(sheet1)
+print(cellA1.value)
 
+print(cellA1.row)
+print(cellA1.column)
+print(cellA1.coordinate)
 
-#create a new worksheet
-wb.create_sheet(index=1,title="Movie Sheet")
+print(sheet1.cell(1,2).value)
 
-#write content to a call
-MySheet["A1"] = "No."
-MySheet["B1"] = "Movie Title"
-MySheet["C1"] = "Release Date"
-MySheet["D1"] = "Gross"
-MySheet["E1"] = "Total Gross"
-MySheet["F1"] = "Percent of Total Gross"
+for i in range(1,sheet1.max_row+1):
+    print(sheet1.cell(i,2).value)
 
-#change the font size and italicize
-MySheet["A1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
-MySheet["B1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
-MySheet["C1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
-MySheet["D1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
-MySheet["E1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
-MySheet["F1"].font = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
+print(sheet1.max_row)
+print(sheet1.max_column)
 
-#alternatively you can create a Font object and assign it 
+print(xl.utils.get_column_letter(1))
+print(xl.utils.get_column_letter(900))
 
-fontobject = Font(name = "Time New Roman", size = 24, italic = True, bold = True)
+print(xl.utils.column_index_from_string('AHP'))
 
-MySheet["A1"].font = fontobject
+for currentrow in sheet1["A1":"C3"]:
+    print(currentrow)
+    for currentcell in currentrow:
+        print(currentcell.coordinate, currentcell.value)
+        print("---END OF COLUMNS---")
 
-#adding values to cells 
-MySheet["A2"] = 1
-MySheet["A3"] = 2
-MySheet["A4"] = 3
-MySheet["A5"] = 4
-MySheet["A6"] = 5
+    print()
+    print("---END OF ROW---")
+    print()
 
-
-MySheet["A6"] = "Total"
-MySheet["A5"].font = Font(size=16, bold= True)
-
-MySheet["B6"] = "=SUM(B2:B4)"
-
-#change the column width
-MySheet.column_dimensions["A"].width = 25 
-
-wb.save("PythonToExcel.xlsx")
+for currentrow in sheet1.iter_rows(min_row = 1, max_row = sheet1.max_row, max_col = sheet1.max_column):
+    print(currentrow[0].value)
+    print(currentrow[1].value)
+    print(currentrow[2].value) 
